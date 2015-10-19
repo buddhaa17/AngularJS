fff936b509077b09e996c9a3422b1acb96e53324
bkamble <bkamble@tavisca.com>
2015-10-19 16:24:43 +0530

    Added the scope inheritance code

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,24 @@
 angular.module('myControllers',[]).controller('testController',['$scope',function($scope){
 	$scope.title = "Welcome To AngularJs"
+	$scope.double= function(value){	return 2*value;};
+
 }]);
+
+var myModule = angular.module('myControllers');
+
+myModule.controller('parentController',['$scope',function($scope){
+	$scope.name="Ravi";
+	$scope.timeoftheDay="Good Morning";	
+}]);
+
+myModule.controller('childController',['$scope',function($scope){
+	$scope.name="Raj";
+}]);
+
+
+myModule.controller('grandChildController',['$scope',function($scope){
+	$scope.name="RaviRaj";
+	$scope.timeoftheDay="Good evening";
+}]);
+
+
