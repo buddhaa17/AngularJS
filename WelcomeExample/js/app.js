4f8873e092bc4cc13318b2d584972a81d754dbc2
Manoj Shevate <manoj.shevate@gmail.com>
2015-10-22 23:17:15 +0530

    Added router instead of loading new page

--- WelcomeExample/js/app.js
+++ WelcomeExample/js/app.js
@@ -1,1 +1,12 @@
-angular.module('myApp',['myControllers']);
+var myApp = angular.module('myApp',['myControllers','ngRoute']);
+
+myApp.config(['$routeProvider',function($routeProvider){
+	$routeProvider.when('/inheritance',{
+		templateUrl: 'Inheritance.html'
+	}).when('/home',{
+		templateUrl : 'Double.html',
+		controller: 'testController'
+	}).otherwise({
+		redirectTo:'/Home'
+	})
+}]);
